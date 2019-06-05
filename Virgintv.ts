3e0007764bfbaa2192c61045ed72b73be02edd04
GladistonXD <50533550+GladistonXD@users.noreply.github.com>
2019-06-05 04:24:52 -0300

    Update and rename Virgintv.m3u8 to Virgintv.ts

--- Virgintv.ts
+++ Virgintv.ts
@@ -1,6 +1,5 @@
 #EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="aac",NAME="Original",DEFAULT=YES,AUTOSELECT=YES,URI="https://live3-mediaset-it.akamaized.net/Content/hls_h0_clr_vos/live/channel(ew)/stream04/streamPlaylist.m3u8"
-#EXT-X-MEDIA:TYPE=AUDIO,GROUP-ID="aac",LANGUAGE="en",NAME="English",DEFAULT=NO,AUTOSELECT=YES,URI="https://live3-mediaset-it.akamaized.net/Content/hls_h0_clr_vos/live/channel(ew)/stream05/streamPlaylist.m3u8"
 #EXT-X-I-FRAME-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=28451,CODECS="avc1.4d400d",URI="https://live3-mediaset-it.akamaized.net/Content/hls_h0_clr_vos/live/channel(ew)/stream04/streamPlaylist.m3u8"
 #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=577610,CODECS="mp4a.40.2, avc1.4d401e",RESOLUTION=640x360,AUDIO="bipbop_audio",SUBTITLES="subs"
