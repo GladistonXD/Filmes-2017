56eb3abff500ac9a643ecdccc6acabb9c16957e6
GladistonXD <50533550+GladistonXD@users.noreply.github.com>
2019-08-01 06:01:33 -0300

    Update canaisvibgratis.php

--- canaisvibgratis.php
+++ canaisvibgratis.php
@@ -1,1 +1,1 @@
- https://d1lud49pck8hpm.cloudfront.net/hls1/.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9Ny8zMC8yMDE5IDU6NDY6NDggUE0maGFzaF92YWx1ZT1leUlTYXNNcEJLSFQyWktCQXdDUG9RPT0mdmFsaWRtaW51dGVzPTYwJmlkPTU=",
+chãonimãhttps://d1lud49pck8hpm.cloudfront.net/hls1/.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9Ny8zMC8yMDE5IDU6NDY6NDggUE0maGFzaF92YWx1ZT1leUlTYXNNcEJLSFQyWktCQXdDUG9RPT0mdmFsaWRtaW51dGVzPTYwJmlkPTU=",
